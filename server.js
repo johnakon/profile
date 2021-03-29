05472c91c39d60a169883f6510c6f65dd91c2891
Akonya John <johnakon>
2021-03-29 13:03:51 +0300

    deploying to heroku

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express")
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('I am a full stack dev')
 })
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('profile/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'profile', 'build', 'index.html'))
+    })
+}
+
 
 const PORT = process.env.PORT || 3000;
 
